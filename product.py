ad3ad4a33587996023aa4b5b41fbd35219adbd7d
bo cheng <cgs05521@gmail.com>
2019-12-30 19:56:05 +0800

    new one

--- product.py
+++ product.py
@@ -1,13 +1,28 @@
+#讀取檔案
+products = []
+with open('product.csv' , 'r' , encoding='utf-8')as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name,price])
+print(products)
 
-product = []
+#使用者輸入
+products = []
 while True:
 	name = input('請輸入商品名稱： ')
 	if name == 'q' :
 		break
 	price = input('請輸入商品價格')
-	product.append([name,price])
-for p in product:
+	products.append([name,price])
+print(products)
+
+#印出所有購買紀錄
+for p in products:
 	print(p[0],'的價格是',p[1])
-with open('product.txt', 'w') as f:
-	for p in product:
+
+#寫入檔案
+with open('product.csv', 'w',encoding='utf-8') as f:
+	for p in products:
 		f.write(p[0]+','+p[1]+'\n')
